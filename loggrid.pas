3d729ba9d078c606b7ef59d84cfe4fd1c66204d3
jwdietrich <jwdietrich@05b22157-dbde-4095-8ba5-0db5c606093b>
2021-07-05 20:05:30 +0000

    Improving handling of simulated time.

--- loggrid.pas
+++ loggrid.pas
@@ -31,7 +31,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Grids,
-  SimulationEngine;
+  SimulaBetaServices, SimulationEngine;
 
 type
 
@@ -81,13 +81,14 @@ begin
   for i := 0 to maxRow - 1 do
   begin
     ValuesGrid.Cells[0, i + 1] := IntToStr(i + 1);
-    ValuesGrid.Cells[1, i + 1] := FloatToStrF(gValues.P[i] / PFactor, ffFixed, 0, 4);
-    ValuesGrid.Cells[2, i + 1] := FloatToStrF(gValues.R[i] / MicroFactor, ffFixed, 0, 4);
-    ValuesGrid.Cells[3, i + 1] := FloatToStrF(gValues.G[i] / GFactor, ffFixed, 0, 4);
-    ValuesGrid.Cells[4, i + 1] := FloatToStrF(gValues.S[i] / PicoFactor, ffFixed, 0, 4);
-    ValuesGrid.Cells[5, i + 1] := FloatToStrF(gValues.I[i] / IFactor, ffFixed, 0, 4);
-    ValuesGrid.Cells[6, i + 1] := FloatToStrF(gValues.M[i], ffFixed, 0, 4);
-    ValuesGrid.Cells[7, i + 1] := FloatToStrF(gValues.N[i], ffFixed, 0, 4);
+    ValuesGrid.Cells[1, i + 1] := FormattedTime(gValues.t[i]);
+    ValuesGrid.Cells[2, i + 1] := FloatToStrF(gValues.P[i] / PFactor, ffFixed, 0, 4);
+    ValuesGrid.Cells[3, i + 1] := FloatToStrF(gValues.R[i] / MicroFactor, ffFixed, 0, 4);
+    ValuesGrid.Cells[4, i + 1] := FloatToStrF(gValues.G[i] / GFactor, ffFixed, 0, 4);
+    ValuesGrid.Cells[5, i + 1] := FloatToStrF(gValues.S[i] / PicoFactor, ffFixed, 0, 4);
+    ValuesGrid.Cells[6, i + 1] := FloatToStrF(gValues.I[i] / IFactor, ffFixed, 0, 4);
+    ValuesGrid.Cells[7, i + 1] := FloatToStrF(gValues.M[i], ffFixed, 0, 4);
+    ValuesGrid.Cells[8, i + 1] := FloatToStrF(gValues.N[i], ffFixed, 0, 4);
   end;
   ValuesGrid.EndUpdate(true);
 end;
