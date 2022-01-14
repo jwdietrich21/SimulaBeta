bea7e77853e81baff42a0d0051216bf52c2a22ca
jwdietrich <jwdietrich@05b22157-dbde-4095-8ba5-0db5c606093b>
2022-01-14 20:33:47 +0000

    Preparing the development of SimulaBeta 2.1.0 (Turning the tides).

--- simulationcontrol.pas
+++ simulationcontrol.pas
@@ -5,12 +5,12 @@ unit SimulationControl;
 { A simulator for insulin-glucose homeostasis }
 { Control Window for overall simulation }
 
-{ Version 2.0.0 (Sleeping Briar Rose) }
+{ Version 2.1.0 (Turning the tides) }
 
-{ (c) Johannes W. Dietrich, 1994 - 2021 }
+{ (c) Johannes W. Dietrich, 1994 - 2022 }
 { (c) Ludwig Maximilian University of Munich 1995 - 2002 }
 { (c) University of Ulm Hospitals 2002-2004 }
-{ (c) Ruhr University of Bochum 2005 - 2021 }
+{ (c) Ruhr University of Bochum 2005 - 2022 }
 
 { Standard blocks for systems modelling and simulation }
 
